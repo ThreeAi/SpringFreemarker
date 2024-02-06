2d24d455f74369aefce65db2670d15a05a325fa1
ThreeAi <alexandrovaa2003@gmail.com>
2024-02-07 00:31:47 +0300

    first_lesson_init_project

---
+++ src/main/java/com/petproject/SpringFreemarker/SpringFreemarkerApplication.java
@@ -0,0 +1,13 @@
+package com.petproject.SpringFreemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFreemarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFreemarkerApplication.class, args);
+	}
+
+}

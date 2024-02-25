caa52bbde5770c6f262dfcd03c66c21ee23ab9f2
ThreeAi <alexandrovaa2003@gmail.com>
2024-02-26 02:11:01 +0300

    fifth_lesson add deprecated spring security

---
+++ src/main/java/com/petproject/SpringFreemarker/configurations/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.petproject.SpringFreemarker.configurations;
+
+import com.petproject.SpringFreemarker.services.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfiguration {
+
+    private final CustomUserDetailsService customUserDetailsService;
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/registration").permitAll()
+                        .requestMatchers("/product/**", "/image/**")
+                        .hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(8);
+    }
+
+}
---
+++ src/main/java/com/petproject/SpringFreemarker/controllers/UserController.java
@@ -0,0 +1,38 @@
+package com.petproject.SpringFreemarker.controllers;
+
+import com.petproject.SpringFreemarker.models.User;
+import com.petproject.SpringFreemarker.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/registration")
+    public String createUser(User user, Model model) {
+        if (!userService.createUser(user)) {
+            model.addAttribute("errorMessage", "User with email: " + user.getEmail() + " already exist");
+            return "registration";
+        }
+        return "redirect:/login";
+    }
+
+    @GetMapping("/hello")
+    public String securityUrl() {
+        return "hello";
+    }
+}
